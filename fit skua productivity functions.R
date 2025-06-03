6ea193f9aea1a5af29ff3008cccdae2e276da3ba
wfmorris <52839019+wfmorris@users.noreply.github.com>
2025-06-03 13:54:01 -0400

    Update fit skua productivity functions.R

--- fit skua productivity functions.R
+++ fit skua productivity functions.R
@@ -1,5 +1,8 @@
-# This code accompanies a manuscript entitled 
-# "What is demographic lability and when might we expect to see it?"
+# This code accompanies the following paper:
+
+# Morris, W.F., and D.F. Doak. 2025. "What is demographic lability and when might we
+# expect to see it? American Naturalist, in press.
+
 # It uses data obtained by digitizing Fig. 2 in Barraquand et al., 2014, 
 # Journal of Animal Ecology. The data (found in the file 
 # "skua data from Barraquand.csv") describes the relationship between fecundity
